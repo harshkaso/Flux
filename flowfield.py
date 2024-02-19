d9c58796e6cb0c0025fb37e05c93222df29cbdb4
harshkaso <extraform8@gmail.com>
2024-02-19 13:21:58 -0500

    Optimize particles

--- flowfield.py
+++ flowfield.py
@@ -1,20 +1,22 @@
 import dearpygui.dearpygui as dpg
-from opensimplex import noise3, noise3array
+from opensimplex import noise3array, random_seed
 from particle import Particle
 import numpy as np
 
-_scale = 50
-_cols = 20
-_rows = 10
-particles_total = 500
+_scale = 15
+_cols = 40
+_rows = 20
+particles_total = 1000
+_two_pi = np.pi * 2
 
 _width = _cols * _scale
 _height = _rows * _scale
 _x = np.arange(_cols)/_cols
 _y = np.arange(_rows)/_rows
 _bg_color = [1,5,58,255]
+random_seed()
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=_width, height=_height, resizable=False)
+dpg.create_viewport(title='Flux', width=_width, height=_height, resizable=False)
 dpg.setup_dearpygui()
 
 z = 0
@@ -31,15 +33,12 @@ def recalc_particles():
     for particle in particles:
         x = (particle.pos[0] // _scale) % _cols
         y = particle.pos[1] // _scale
-        # idx = int(x + y * _cols)
-        try:
-            angle = flowfield[int(y)][int(x)]
-            particle.follow((np.cos(angle), np.sin(angle)))
-            particle.update()
-            particle.warp_around_edges(_width, _height)
-        except Exception as e:
-            print(e )
+        angle = flowfield[int(y)][int(x)]
+        particle.apply_force((np.cos(angle), np.sin(angle)))
+        particle.update()
+        particle.warp_around_edges(_width, _height)
     z += inc
+
 
 def _handle_frame_buffer(sender, buffer):
     with dpg.mutex():
@@ -54,7 +53,7 @@ def _handle_frame_buffer(sender, buffer):
                 dpg.add_image('prev_frame', parent='flowfield', pos=(0,0))
                 # Adding a dimmer - once and for good
                 _background(opacity=10)
-
+                
             # We've stored current picture into the background texture and
             # are now ready to move particles around.
             recalc_particles()
@@ -63,21 +62,11 @@ def _handle_frame_buffer(sender, buffer):
             dpg.set_frame_callback(dpg.get_frame_count()+2, callback=lambda: dpg.output_frame_buffer(callback=_handle_frame_buffer))
 
 def _flowfield(z):
-    global _x, _y
-    angles = noise3array(_x, _y, np.array([z]))[0] * np.pi * 2
-    # flowfield = []
-    # for y in _y:
-    #     for x in _x:
-    #         r = noise3(x, y, z)
-    #         angle = r * np.pi * 2
-    #         flowfield.append((np.cos(angle), np.sin(angle)))
-    # flowfield = np.array((np.cos(angles), np.sin(angles)))
-    return angles
+    global _x, _y, _two_pi
+    return noise3array(_x, _y, np.array([z]))[0] * _two_pi
+    
 
-def _background(clr=None, opacity=255):
-    global _bg_color
-    if clr == None:
-        clr = _bg_color[:3]
+def _background(clr=_bg_color[:3], opacity=255):
     x, y = dpg.get_viewport_client_width(), dpg.get_viewport_client_height()
     clr.append(opacity)
     background = dpg.draw_rectangle(pmin=(0,0), pmax=(x, y), parent='flowfield', fill=clr, color=clr)
@@ -86,15 +75,13 @@ def _background(clr=None, opacity=255):
 
 with dpg.window(label="FlowField", tag='flowfield', width=_width, height=_height):
     dpg.set_primary_window('flowfield', True)
-    
-    with dpg.theme() as flowfield_theme:
-            with dpg.theme_component(dpg.mvAll):
-                dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 0)
-                # dpg.add_theme_color(dpg.mvThemeCol_ChildBg, _bg_color, category=dpg.mvThemeCat_Core)
-    dpg.bind_item_theme('flowfield', flowfield_theme)
-
     particles = np.array([ Particle(parent = 'flowfield', pos = [np.random.random() * _width, np.random.random() * _height]) for i in range(particles_total) ])
 
+with dpg.theme() as flowfield_theme:
+        with dpg.theme_component(dpg.mvAll):
+            dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 0)
+            dpg.add_theme_color(dpg.mvThemeCol_ChildBg, _bg_color, category=dpg.mvThemeCat_Core)
+dpg.bind_item_theme('flowfield', flowfield_theme)
 
 dpg.show_viewport()
 dpg.set_frame_callback(20, callback=lambda: dpg.output_frame_buffer(callback=_handle_frame_buffer))
