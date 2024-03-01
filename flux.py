963c2fbb04e566b33930723f086474a477d33f5e
harshkaso <extraform8@gmail.com>
2024-03-01 00:09:51 -0500

    add configurable parameters to side panel with custom dropdown

--- flux.py
+++ flux.py
@@ -9,14 +9,14 @@ TAU = np.pi * 2
 sp_width = 250          # Side Panel Width
 ff_width  = 1000        # Flowfield Width
 ff_height = 750         # Flowfield Height
-n_scale = .1           # Noise Scale
-t_scale =  0.01         # Time Scale
+n_scale = 0.1           # Noise Scale
+t_scale = 0.01          # Time Scale
 
 ## PARTICLE CONFIG
 ttl_particles = 1000    # Total Particles
-min_age = 150            # Max Age of Particles
+min_age = 50           # Max Age of Particles
 max_age = 250           # Min Age of Particles
-speed = 1
+speed = 1               # Speed of particles
 
 bg_color = [1,5,58,255] # Background Color
 
@@ -56,6 +56,8 @@ def recalc_particles():
         p[1] += np.sin(a) * speed
         p[2] -= 1
         if not (p[0] > 0 and p[0] < ff_width and p[1] > 0) or p[2] == 0:
+            # if particle is not (on-screen) or age == 0
+            # reset the particle 
             p[0] = np.random.random() * ff_width
             p[1] = np.random.random() * ff_height
             p[2] = np.random.randint(min_age, max_age)
@@ -79,6 +81,7 @@ def init_frame_buffer(sender, buffer):
         # Setup Initial Frame
         with dpg.texture_registry():
             dpg.add_raw_texture(width=w_width, height=w_height, default_value=buffer, format=dpg.mvFormat_Float_rgba, tag="prev_frame")
+            # dpg.add_dynamic_texture(width=w_width, height=w_height, default_value=buffer, tag="prev_frame")
         dpg.add_image('prev_frame', width=ff_width, parent='flowfield', pos=(0,0), uv_min=(0,0), uv_max=(ff_width/w_width, 1))
         background(opacity=10)
         spawn_paricles()
@@ -99,6 +102,34 @@ def setup_flux():
     dpg.create_viewport(title='Flux', width=ff_width+sp_width, height=ff_height, resizable=False)
     dpg.setup_dearpygui()
 
+    # Callbacks
+    def set_n_scale(sender, data):
+        global n_scale
+        n_scale = data
+
+    def set_t_scale(sender, data):
+        global t_scale
+        t_scale = data
+
+    def set_particle_speed(sender, data):
+        global speed
+        speed = data
+
+    def set_min_max_age(sender, data):
+        global min_age, max_age
+        if sender == 'min-age':
+            min_age = data
+        elif sender == 'max-age':
+            max_age = data
+
+    def handle_dropdown(sender, data, group):
+        if dpg.is_item_shown(group):
+            dpg.configure_item(sender, direction=dpg.mvDir_Right)
+            dpg.configure_item(group, show=False)
+        else:
+            dpg.configure_item(sender, direction=dpg.mvDir_Down)
+            dpg.configure_item(group, show=True)
+
     with dpg.window(tag='flowfield', pos=(0,0)):
         dpg.set_primary_window('flowfield', True)
         with dpg.theme() as flowfield_theme:
@@ -113,6 +144,23 @@ def setup_flux():
                 with dpg.theme_component(dpg.mvAll):
                     dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 8)
             dpg.bind_item_theme('parameters', side_panel_theme)
+            # Settings for parameters
+            dpg.add_spacer(height=3)
+            with dpg.group(horizontal=True):
+                dpg.add_button(tag='ff-dropdown', arrow=True, direction=dpg.mvDir_Down, callback=handle_dropdown, user_data='flowfield-settings')
+                dpg.add_text(default_value='Flowfield Properties')
+            with dpg.group(tag='flowfield-settings'):
+                dpg.add_slider_float(width=sp_width/2, label='noisescale', min_value=0.05, default_value=n_scale, max_value=3, callback=set_n_scale)
+                dpg.add_slider_float(width=sp_width/2, label='timescale', min_value=0, default_value=t_scale, max_value=0.1, callback=set_t_scale)
+            dpg.add_separator()
+            with dpg.group(horizontal=True):
+                dpg.add_button(tag='pp-dropdown', arrow=True, direction=dpg.mvDir_Down, callback=handle_dropdown, user_data='particle-settings')
+                dpg.add_text(default_value='Particle Properties')
+            with dpg.group(tag='particle-settings'):
+                dpg.add_slider_float(width=sp_width/2, label='speed', min_value=0.5, default_value=speed, max_value=4, callback=set_particle_speed)
+                dpg.add_slider_int(width=sp_width/2, label='min age', tag='min-age', min_value=min_age, default_value=min_age, max_value=100, callback=set_min_max_age)
+                dpg.add_slider_int(width=sp_width/2, label='max age', tag='max-age', min_value=101, default_value=max_age, max_value=max_age, callback=set_min_max_age)
+            dpg.add_separator()
     
 def start_flux():
     # Start Flux
@@ -120,8 +168,8 @@ def start_flux():
     dpg.show_viewport()
     dpg.set_frame_callback(20, callback=lambda: dpg.output_frame_buffer(callback=init_frame_buffer))
     # dpg.set_frame_callback(20, callback=lambda: dpg.output_frame_buffer(callback=_handle_frame_buffer))
-    # dpg.set_viewport_vsync(False)
-    dpg.show_metrics()
+    dpg.set_viewport_vsync(False)
+    # dpg.show_metrics()
     dpg.start_dearpygui()
     dpg.destroy_context()
 
