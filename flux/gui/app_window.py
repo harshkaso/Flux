42af9ffee95f4dd153f86cb2e8e7d5343052a4a1
Harsh Kasodariya <harshkasodariya1999@gmail.com>
2026-08-10 20:58:07 -0400

    refractor: clean up code to accordance to new modules

--- flux/gui/app_window.py
+++ flux/gui/app_window.py
@@ -1,24 +1,22 @@
 import dearpygui.dearpygui as dpg  # type: ignore
 from flux.core.types import ItemTag
 from flux.gui.canvas import Canvas
-from flux.gui.sidebar import Sidebar
+from flux.gui.sidebar.sidebar import Sidebar
+from flux.theme.manager import ThemeManager
 from flux.utils.logger import get_logger
-from flux.core.protocols import AppContext
 from flux.core.enums import ComponentTheme
 
 logger = get_logger(__name__)
 
 
 class AppWindow:
-    def __init__(self, app: AppContext) -> None:
-        self.app = app
+    def __init__(self) -> None:
         self._tag: ItemTag = dpg.generate_uuid()
         with dpg.window(tag=self._tag):
             with dpg.group(horizontal=True, horizontal_spacing=0.0):
-                self.sidebar: Sidebar = Sidebar(self.app)
-                self.canvas: Canvas = Canvas(self.app)
-        self.app.theme.subscribe(subscriber=self)
-        self.apply_theme()
+                self.sidebar: Sidebar = Sidebar()
+                self.canvas: Canvas = Canvas()
+        ThemeManager.subscribe(subscriber=self)
 
     @property
     def tag(self) -> ItemTag:
@@ -26,5 +24,5 @@ class AppWindow:
 
     def apply_theme(self) -> None:
         dpg.bind_item_theme(
-            self._tag, self.app.theme.item_theme(ComponentTheme.APP_WINDOW)
+            self._tag, ThemeManager.item_theme(ComponentTheme.APP_WINDOW)
         )
